3b3837d8a5f65200c22bd04a8a8bb3407d4d432b
ajayarulkumaran5678 <144941913+ajayarulkumaran5678@users.noreply.github.com>
2024-02-25 15:10:47 +0530

    Add files via upload

---
+++ src/Data/FollowersData.js
@@ -0,0 +1,11 @@
+import img1 from "../img/img1.png";
+import img2 from "../img/img2.png";
+import img3 from "../img/img3.png";
+import img4 from "../img/img4.jpg";
+
+export const Followers = [
+  { name: "Arun", username: "Arunmathavan", img: img1 },
+  { name: "Harold", username: "Haroldy", img: img2 },
+  { name: "Tisap", username: "Tis01", img: img3 },
+  { name: "Niraj", username: "Niraj001", img: img4 },
+];
